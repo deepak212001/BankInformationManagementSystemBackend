c7c5cc6479f2f7cf11de8dc93785a7a9218a7df3
Deepak Yadav <yadavdeep212001@gmail.com>
2025-07-24 18:48:20 +0530

    Refactor app.js: clean up formatting and improve code readability

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ import cookiesParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173", 
+    origin: ["http://localhost:5173/", "https://bank-information-management-system-opal.vercel.app/"],
     credentials: true
 }
 ))
 
-    app.use(express.json({ limit: '16kb' }))
+app.use(express.json({ limit: '16kb' }))
 // app.use(express.json());
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
@@ -18,8 +18,8 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(cookiesParser())
 
 app.get('/', (req, res) => {
-        res.send("Welcome to the API")
-    })
+    res.send("Welcome to the API")
+})
 
 import userRoutes from "./routes/user.routes.js"
 app.use("/api/user", userRoutes);
