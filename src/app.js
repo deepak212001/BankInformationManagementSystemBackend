8b5b67cbfe0e17a783d07d3da90ce1ad8d810905
Deepak Yadav <yadavdeep212001@gmail.com>
2025-07-24 19:36:42 +0530

    Remove duplicate cookie parser middleware usage in app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import cookiesParser from "cookie-parser"
 
 const app = express()
+app.use(cookiesParser())
 
 app.use(cors({
     origin: ["http://localhost:5173", "https://bank-information-management-system-opal.vercel.app"],
@@ -15,7 +16,7 @@ app.use(express.json({ limit: '16kb' }))
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 
-app.use(cookiesParser())
+
 
 app.get('/', (req, res) => {
     res.send("Welcome to the API")
